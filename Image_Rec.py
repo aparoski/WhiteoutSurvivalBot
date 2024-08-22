860455aa2b2fe671c4f8b9b974d2ae619d05664d
Anton Paroski <Anton_Paroski@Alumni.baylor.edu>
2024-08-22 12:51:32 -0500

    fix to the event_check button. add delay to account for slippery slider

--- Image_Rec.py
+++ Image_Rec.py
@@ -102,6 +102,8 @@ def find_event(x1, y1, W, L, path, message = "icon"):
 
         HF.swipe(x1, y1, W, L, dir = "right", starting_y = 0.15, magnitude = 1,
                  manual_duration = 1)
+        
+        time.sleep(0.5)
 
     if icon:
 
@@ -124,7 +126,7 @@ def Hero_Mission(x1, y1, W, L) -> None:
 
     Hero_Mission_loc = find_event(x1, y1, W, L, dir + hero_mission_icon)
     #need to wait on slider to finish slipping...
-    time.sleep(0.5)
+    
 
     p.click(Hero_Mission_loc)
 
