1685d04cc31f29f815f441ae47b2ff3244b6970b
Anton Paroski <Anton_Paroski@Alumni.baylor.edu>
2024-09-01 23:49:56 -0500

    playing around with schedule ideas.

--- Master_Scheduler.py
+++ Master_Scheduler.py
@@ -6,6 +6,8 @@ import time
 import Window_Finder
 import Reader
 import Image_Rec
+import Data
+import Map_Interact
 
 #temp for testing
 import pyautogui as p
@@ -17,12 +19,38 @@ Tootin = "BlueStacks App Player 1"
 Tootily = "BlueStacks App Player 3"
 Leg = "BlueStacks App Player 4"
 
-#function to read the schedule
-def schedule_reader():
-    pass
+schedule = Data.Window_Dataframe()
+
+polar_rally = "Polar_Rally"
+beast_hunt = "Beast_Hunt"
+Troop_Training = "Troop_Training" + "Troop_Type"
+
+beast_count = 0
+
+leg = Window_Finder.BlueStack_Window(Leg)
+
+x1, y1, x2, y2 = leg.rectangle
+
+W, L = leg.W_L
+
+#play around with a beast hunt to figure out scheduling 
+while beast_count < 27:
+
+    error_int = 0
+
+    leg.march_time = Map_Interact.Beast_Search(x1, y1, W, L, 21)
+
+    schedule.add(leg.name, leg.hwnd, beast_hunt, leg.march_time, "s")
+
+    #scan schedule for when beast returns
+    while error_int < 5000:
+
+        
+
+        
 
 
-def beast_hunt_schedule(x1, y1, W, L, accounts):
-    pass
+
+
 
 
